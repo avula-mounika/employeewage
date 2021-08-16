1c1ccb8b8a5fee2eef138af9cf84900edc920465
avula-mounika <goudmounika13@gmail.com>
2021-08-16 14:35:45 +0530

    welcome

---
+++ Employeewelcome.java
@@ -0,0 +1,10 @@
+package employee;
+
+public class Employeewelcome {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage computation program");// TODO Auto-generated method stub
+
+	}
+
+}
